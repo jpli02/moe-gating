6cd397cf966c56ccf63731b2bfb3e693656a474c
jpli02 <jplee0202@gmail.com>
2025-02-25 19:11:46 -0600

    update

--- build/lib/megablocks/_version.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2024 Databricks
-# SPDX-License-Identifier: Apache-2.0
-
-"""The MegaBlocks Version."""
-
-__version__ = '0.8.0.dev0'
--- build/lib/megablocks/backend/__init__.py
+++
@@ -1,2 +0,0 @@
-# Copyright 2024 Databricks
-# SPDX-License-Identifier: Apache-2.0
