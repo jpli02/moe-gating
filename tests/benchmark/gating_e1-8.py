3d4bc4282b63992cdff18ea68ea4ed43cd224ae0
Jianping Li <jli199@illinois.edu>
2025-03-01 17:48:29 +0000

    new scripts

--- tests/benchmark/gating_e1-8.py
+++ tests/benchmark/gating_e1-8.py
@@ -1,8 +1,10 @@
 import os
 
 expert = [1, 2, 4, 8]
-
+top = [2, 4, 8]
 for ex in expert:
-    command = f"python ../megablocks/layers/benchmark.py --top_k 1 --e {ex} --s 4096 --hid_dim 7168 --bs 1"
-    print(f"Running: {command}")
-    os.system(command)         
+    for t in top:
+        if ex >= t:
+            command = f"python ../../megablocks/layers/benchmark.py --top_k {t} --e {ex} --s 4096 --hid_dim 7168 --bs 1"
+            print(f"Running: {command}")
+            os.system(command)         
