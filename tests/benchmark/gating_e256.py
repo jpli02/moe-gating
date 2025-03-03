9176da28c53107d08bcdf27dc07b0453e4c76480
Jianping Li <jli199@illinois.edu>
2025-03-03 17:57:14 +0000

    scripts

--- tests/benchmark/gating_e256.py
+++ tests/benchmark/gating_e256.py
@@ -3,6 +3,6 @@ import os
 top_k_values = [2, 4, 6, 8, 10, 12, 32, 64]
 
 for top_k in top_k_values:
-    command = f"python ../megablocks/layers/benchmark.py --top_k {top_k} --e 256 --s 4096 --hid_dim 1024 --bs 1"
+    command = f"python ../../megablocks/layers/benchmark.py --top_k {top_k} --e 128 --s 4096 --hid_dim 7168 --bs 1"
     print(f"Running: {command}")
     os.system(command)         
