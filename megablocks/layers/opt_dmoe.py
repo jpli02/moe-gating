64fcadf78cdf14a7b7498a047e12b1327cb327f9
Ahan Gupta <ahangupta.96@gmail.com>
2025-03-15 20:34:01 +0000

    runnable end to end, however semantic bugs not fixed

--- megablocks/layers/opt_dmoe.py
+++ megablocks/layers/opt_dmoe.py
@@ -183,9 +183,8 @@ if __name__ == '__main__':
         topk_weights, topk_args = torch.topk(sm(torch.randn((batch_size*num_tokens, num_experts), device="cuda" if torch.cuda.is_available() else "cpu")), k=num_experts)
         opt_res = optMoE.forward_once(x, topk_weights, topk_args)
         ground_truth = paddedMoE.forward_once(x, topk_weights, topk_args)
-        pdb.set_trace()
 
-        print(f'max diff: {torch.abs(opt_res - ground_truth).max().item()}')
+        print(f'max diff: {torch.abs(opt_res[0] - ground_truth[0]).max().item()}')
     """
     Certain constraints on megablocks:
         - m and n and k need to be multiples of 128.
