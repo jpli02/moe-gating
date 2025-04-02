547f9aa640da3c2c5d65f6febe1bbd2d91124156
Ahan Gupta <ag82@gpud01.delta.ncsa.illinois.edu>
2025-04-01 23:53:36 -0500

    newer benchmark

--- megablocks/layers/microbenchmark.py
+++ megablocks/layers/microbenchmark.py
@@ -109,8 +109,8 @@ def irregular_split(num_tokens: int, num_experts: int):
     return token_dist + [num_tokens - total_token_cnt]
 
 if __name__ == '__main__':
-    #token_cnt = [1024, 2048, 4096, 8192, 16384, 32768]
-    token_cnt = [1024, 2048, 4096, 16384, 32768, 16384]
+    token_cnt = [1024, 2048, 4096, 8192, 16384, 32768]
+    #token_cnt = [1024, 2048, 4096, 16384, 32768, 16384]
     #inner_dimensions = [(2048, 1408), (5120, 1536), (7168, 2048)]
     inner_dimensions = [(7168, 2048)]
     #inner_dimensions = [(5120, 1536)]
