f31506919e06900691b003d85cb091088fd2213a
Ahan Gupta <ahangupta.96@gmail.com>
2025-04-02 17:21:55 +0000

    balanced benchmark

--- megablocks/layers/microbenchmark.py
+++ megablocks/layers/microbenchmark.py
@@ -117,7 +117,7 @@ if __name__ == '__main__':
     #inner_dimensions = [(2048, 1408)]
     args = Arguments()
     num_experts = 32
-    ratio = 64
+    ratio = 2
     for tc in token_cnt:
         for hid_dim, ffn_dim in inner_dimensions:
             test_grouped_gemm(tc, hid_dim, num_experts, 8, ffn_dim, torch.float16, args, two_two_split(ratio, tc, num_experts))
